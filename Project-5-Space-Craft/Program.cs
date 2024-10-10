5f11f676ae4395486c15ad90b19059f46d7d7bc0
Sean <stank3391@conestogac.on.ca>
2024-10-10 13:16:38 -0400

    Implements the spaceship and Packetwrapper class.

--- Project-5-Space-Craft/Program.cs
+++ Project-5-Space-Craft/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Project_5_Space_Craft;
+
+
+
 Console.WriteLine("Hello, Payload Ops!");
+
+Spaceship s = new Spaceship();
+s.Add(new ReadingsStub());
+Console.ReadLine();
---
+++ Project-5-Space-Craft/Spaceship.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Project_5_Space_Craft
+{
+    internal class Spaceship
+    {
+        private List<SpaceshipReadings> data;
+        private Timer timer;
+        public Spaceship() {
+            this.data = new List<SpaceshipReadings>();
+
+            DateTime now = DateTime.Now;
+#if DEBUG
+            // For DEBUG: Set to trigger every minute
+            DateTime nextEvent = now.AddMinutes(1).AddSeconds(-now.Second).AddMilliseconds(-now.Millisecond);
+            Console.WriteLine($"Timer set to trigger every minute in DEBUG mode.");
+            TimeSpan eventLength = TimeSpan.FromMinutes(1);
+#else
+            // For RELEASE: Set to trigger every hour
+            DateTime nextEvent = now.AddHours(1).AddMinutes(-now.Minute).AddSeconds(-now.Second).AddMilliseconds(-now.Millisecond);
+            Console.WriteLine($"Timer set to trigger every hour in RELEASE mode.");
+            TimeSpan eventLength = TimeSpan.FromHours(1);
+#endif
+            this.timer = new Timer(this.TimedEvent, null, nextEvent - now, eventLength);
+        }
+
+        public Spaceship Add(SpaceshipReadings data)
+        {
+            this.data.Add(data);
+            return this;
+        }
+
+        private void TimedEvent(object? state)
+        {
+            Console.WriteLine("Event triggered at: " + DateTime.Now);
+            //Need to spin up some packet wrappers \o/
+            foreach (SpaceshipReadings singleData in this.data)
+            {
+                int num = Send(new PacketWrapper(singleData));
+            }
+
+        }
+
+        private int Send(PacketWrapper packet)
+        {
+
+            //This function is blocked by uplink downlink
+            //return uplink.downlink.send(packet)
+            foreach (KeyValuePair<string, string> entry in packet.ToJson())
+            {
+                // do something with entry.Value or entry.Key
+                Console.WriteLine(entry.Key + " : " + entry.Value + ",");
+            }
+            return 1;
+        }
+
+        private void UnpackMessage()
+        {
+            //This function is blocked by uplink downlink
+            //switch per type of message and handle
+        }
+    }
+}
