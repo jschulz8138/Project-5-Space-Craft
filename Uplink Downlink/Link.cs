d6a0625219dd107cbba27330b7a0a39b8935dedc
Yuval Rashish <yrashish3335@conestogac.on.ca>
2024-10-28 14:51:48 -0400

    Added request types to sending requests in the link server

--- Uplink Downlink/Link.cs
+++ Uplink Downlink/Link.cs
@@ -1,20 +1,51 @@
-﻿using RestSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Threading.Tasks;
+using RestSharp;
 
 namespace Uplink_Downlink
 {
+    public enum ReqType
+    {
+        GET, POST, PUT, DELETE
+    };
+
     public class Link
     {
-        private RestClient _client;
-        private string _url;
-
+        private readonly RestClient _client;
+        private readonly string _url;
         public Link(string url)
         {
-            this._url = url;
+            _url = url;
             _client = new RestClient(url);
+        }
+
+        // Function requires the type of request being sent (Get, Post, Put, Delete), then it requires the endpoint that its being sent to, then the last paremeter is the arguments (needs to be in dictonary object format)
+        public async Task<TResponse?> SendRequestAsync<TResponse>(ReqType type, string endpoint, Dictionary<string, string>? args = null) where TResponse : class, new()
+        {
+            var request = new RestRequest(endpoint);
+
+            // Add parameters to the request
+            if (args != null)
+            {
+                foreach (var arg in args)
+                {
+                    request.AddParameter(arg.Key, arg.Value);
+                }
+            }
+
+            // Determine request type
+            switch (type)
+            {
+                case ReqType.GET:
+                    return await _client.GetAsync<TResponse>(request);
+                case ReqType.POST:
+                    return await _client.PostAsync<TResponse>(request);
+                case ReqType.PUT:
+                    return await _client.PutAsync<TResponse>(request);
+                case ReqType.DELETE:
+                    return await _client.DeleteAsync<TResponse>(request);
+                default:
+                    throw new ArgumentException("Invalid request type");
+            }
         }
     }
 }
