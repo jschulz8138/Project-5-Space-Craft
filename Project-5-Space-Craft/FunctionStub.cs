4a06f2a242e0a144bd00652b40d29a7fd1fa2dd7
wang <ywang0789@conestogac.on.ca>
2024-11-03 11:55:34 -0500

    add stub for IFunction

---
+++ Project-5-Space-Craft/FunctionStub.cs
@@ -0,0 +1,20 @@
+﻿//PayloadOps TODO:// Also remove this stub
+//This is an example stub for an implementation of Functions and commands
+using System.ComponentModel;
+
+namespace Project_5_Space_Craft
+{
+    public class FunctionStub : IFunction
+    {
+        public string GetCommand()
+        {
+            return "Some Command";
+        }
+
+        public void RunCommand()
+        {
+            return;
+        }
+
+    }
+}
