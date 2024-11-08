45af67e11c6deaab8ce2e0a26909548af5b90084
Yuval Rashish <yrashish3335@conestogac.on.ca>
2024-11-07 19:11:31 -0500

    Current update to the client
    Combined work between me and Amy.
    
    Added comments as well that explain the functions and variables hovering your mouse over them.
    
    Next Steps:
    we're looking into making a mock server or something we can use to test that our restsharp functions properly work.
    
    Co-Authored-By: Aman Sidhu <117484137+amansidhu24@users.noreply.github.com>

--- Uplink Downlink/CommunicationHandler.cs
+++ Uplink Downlink/CommunicationHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Uplink_Downlink
 {
-    internal class CommunicationHandler
+    public class CommunicationHandler
     {
+        private readonly Link _link;
+        private const string GroundStationUri = "/api";
+        private const string DataRoute = "/Downlink";
+        public CommunicationHandler(string baseUrl)
+        {
+            _link = new Link(baseUrl);
+        }
+
+        //method to ground station
+
+        /// <summary>
+        /// Sends data to the ground station and returns whether the data was received properly.
+        /// </summary>
+        /// <param name="generalData">The data to be sent as key-value pairs.</param>
+        /// <returns>
+        /// A <see cref="Task{Boolean}"/> representing the asynchronous operation, with a value of 
+        /// <c>true</c> if the data was received successfully; otherwise, <c>false</c>.
+        /// </returns>
+        public async Task<bool>UpdateGroundStationAsync(Dictionary<string, string> generalData) 
+        {
+            var response = await _link.SendRequestAsync<RestResponse>(ReqType.POST, GroundStationUri + DataRoute, generalData);
+            return response != null && response.IsSuccessful;
+        }
     }
 }
